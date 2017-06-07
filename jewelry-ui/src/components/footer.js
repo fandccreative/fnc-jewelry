f1b0e23b15a74f15cb135f25c6862d1ef2a0c936
Francis <fgseneres@gmail.com>
2017-06-07 17:32:42 +0800

    update ui

---
+++ jewelry-ui/src/components/footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Footer extends React.Component {
+	render() {
+		return(
+			<footer id="footer">
+				<section className="main-footer">
+					<div className="container">
+						<div className ="row">
+							<div className="">
+							</div>
+						</div>
+						<div className="connect-with-us">
+							<h3>Connect With Us</h3>
+						</div>
+					</div>
+				</section>
+				<section className="copyright">
+					<p>Copyright &copy; F&amp;C JEWELRY - All Rights Reserved</p>
+				</section>
+			</footer>
+		);
+	}
+}
+export default Footer;
--- jewelry-ui/src/index.js
+++ jewelry-ui/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './assets/css/bootstrap.css';
+import './assets/font-awesome/css/font-awesome.css';
+import './assets/css/style.css';
+import './assets/css/resp.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
